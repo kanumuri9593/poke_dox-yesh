7cdfa0a5d69f1ecb222db171d486ca474607d331
yeshu <kanumuriyeswanth@gmail.com>
2016-01-09 12:24:44 -0500

    Initial Commit

---
+++ Poke-Dox-yesh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poke-Dox-yesh
+//
+//  Created by Yeswanth varma Kanumuri on 1/9/16.
+//  Copyright © 2016 Yeswanth varma Kanumuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
